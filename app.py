6d52f68499c29d6114b216d04c3d015efebd831d
soichi-fujiwara <52342154+soichi-fujiwara@users.noreply.github.com>
2019-09-27 13:06:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@
 
 from flask import Flask, render_template, request, redirect, url_for
 import numpy as np
+from gensim.models import KeyedVectors
+#対義語生成
+import lib_wordRevChange as lw
+
 
 app = Flask(__name__)
 
@@ -25,31 +29,55 @@ def index():
 @app.route('/post', methods=['POST', 'GET'])
 def post():
     title = "こんにちは！"
-    rev_word = "TEST"
-
-    #TEST
-    from google.cloud import storage as gcs
-    import pandas as pd
-
-    bucket_name = 'ml_bucket_01'
-    #fname = 'wiki_tohoku.model'
-    fname = 'sample.txt'
-    project_name = 'My First Project'
-
-    #プロジェクト名を指定してclientを作成
-    client = gcs.Client(project_name)
-
-    #バケット名を指定してbucketを取得
-    bucket = client.get_bucket(bucket_name)
-
-    #Blobを作成
-    blob = gcs.Blob(fname, bucket)
-    content = blob.download_as_string()
-
-    rev_word = content
-    #TEST
     
     if request.method == 'POST':
+
+        rev_word = ""
+        words = request.form['name']
+
+        #TEST------------------------------------------------------------
+        from google.cloud import storage as gcs
+        import pandas as pd
+
+        bucket_name = 'ml_bucket_01'
+        fname = 'wiki_tohoku.model'
+        #fname = 'sample.txt'
+        project_name = 'My First Project'
+
+        #プロジェクト名を指定してclientを作成
+        client = gcs.Client(project_name)
+
+        #バケット名を指定してbucketを取得
+        bucket = client.get_bucket(bucket_name)
+
+        #Blobを作成
+        blob = gcs.Blob(fname, bucket)
+        content = blob.download_as_string()
+
+        #**************************************************************************
+        #model rorded check
+        #**************************************************************************
+        try:
+          if model == '':
+            pass
+          else:
+            #roaded
+            pass
+        except NameError as error:
+          #read model #1
+          model = KeyedVectors.load(content)
+          #read model #2
+          #model = pickle.load(open('./drive/My Drive/NLP/wiki_tohoku_pkl.sav', 'rb'))
+
+        #MAIN
+        words = words[0:15]
+        gyaku = u"逆"
+        inherent_words = '[' + words + ']'
+
+        rev_list = lw.wordRevChange(words,gyaku,inherent_words,model)
+        rev_word = rev_list[1]
+        #TEST------------------------------------------------------------
+        
         name = request.form['name']
         return render_template('index.html',
                                name=name, title=title,rev_word=rev_word)
